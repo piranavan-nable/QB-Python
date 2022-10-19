cd27116d8091caf7b4a0148d9a911e27e71e1671
Piranavan Sivanesan <piranavan@n-able.biz>
2022-10-19 11:35:45 +0530

    Added app dir

---
+++ app/pnl_report.py
@@ -0,0 +1,9 @@
+"""
+This Module is for API consumer-side reporting on QBv3 queries transactions.
+It has "Profit & Loss" data retrival function
+"""
+
+import calendar
+from dateutil.rrule import *
+from dateutil.parser import *
+from datetime import *
